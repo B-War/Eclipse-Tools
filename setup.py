05e7a5b24bdd5179dae946af422e1bec66e01d70
B-War <ben.warwick40@gmail.com>
2024-06-09 16:38:54 +0100

    Created entry points for functions

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='eclipse_tools',
-    version='1.01',
+    version='1.02',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
     ],
+    entry_points={
+        'console_scripts': [
+            'create_ship_type = eclipse_combat.ship_types:create_ship_type',
+            'update_ship_type = eclipse_combat.ship_types:update_ship_type',
+            'list_ship_types = eclipse_combat.ship_types:list_ship_types',
+            'delete_ship_type = eclipse_combat.ship_types:delete_ship_type',
+            'simulate_combat = eclipse_combat.combat:simulate_combat',
+            'simulate_combat_parallel = eclipse_combat.combat:simulate_combat_parallel',
+        ],
+    },
 )
