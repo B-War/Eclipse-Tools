eb097a1a18c6522b7b16b013160da576b923add4
B-War <ben.warwick40@gmail.com>
2024-06-14 11:48:12 +0100

    Corrected version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eclipse_tools',
-    version='1.02',
+    version='1.2',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
